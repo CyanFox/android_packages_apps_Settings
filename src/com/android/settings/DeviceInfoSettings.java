135697e326ada478b9e2eb3c6c6fe394bcca11f4
MNT-Mantas <m.zilius@gmail.com>
2014-09-22 09:20:47 +0300

    Display version

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -106,7 +106,7 @@ public class DeviceInfoSettings extends RestrictedSettingsFragment {
         setStringSummary(KEY_BUILD_NUMBER, Build.ID);
         findPreference(KEY_BUILD_NUMBER).setEnabled(true);
         findPreference(KEY_KERNEL_VERSION).setSummary(getFormattedKernelVersion());
-        setValueSummary(KEY_MOD_VERSION, "ro.cf.version");
+        setValueSummary(KEY_MOD_VERSION, "ro.cf.display.version");
         findPreference(KEY_MOD_VERSION).setEnabled(true);
         setValueSummary(KEY_MOD_BUILD_DATE, "ro.build.date");
         getPreferenceScreen().removePreference(findPreference(KEY_MOD_BUILD_DATE));
